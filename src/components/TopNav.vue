0e1510ff8d7f554321c95169084c9f2c3fd6ded2
Usama Iqbal <usamaiqbal12@hotmail.com>
2026-04-21 00:52:12 -0400

    add Best buy branding and CI/CD pipeline

--- src/components/TopNav.vue
+++ src/components/TopNav.vue
@@ -2,7 +2,7 @@
   <nav>
     <div class="logo">
       <a href="/">
-        <img src="/algonquin-pet-store.png" alt="Algonquin Pet Store Logo">
+        <img src="/algonquin-pet-store.png" alt="Best Buy Store Logo">
       </a>
     </div>
     <button class="hamburger" @click="toggleNav">
