a0f275b2c7f8a0fa23f879f4f53e65a0070b3acd
Usama Iqbal <usamaiqbal12@hotmail.com>
2026-04-22 00:23:09 -0400

    fixing best buy logo

--- src/components/TopNav.vue
+++ src/components/TopNav.vue
@@ -2,7 +2,7 @@
   <nav>
     <div class="logo">
       <a href="/">
-        <img src="/best-buy2.png" alt="Best Buy Store Logo">
+        <img src="/best-buy-log-2.png" alt="Best Buy Store Logo">
       </a>
     </div>
     <button class="hamburger" @click="toggleNav">
